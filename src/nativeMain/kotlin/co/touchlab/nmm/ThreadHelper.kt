30a88b1c2d1719263fd48aeb69f473870ef5f548
Kevin Galligan <kgalligan@gmail.com>
2021-10-02 11:19:14 -0400

    Basic test support

--- src/nativeMain/kotlin/co/touchlab/nmm/ThreadHelper.kt
+++ src/nativeMain/kotlin/co/touchlab/nmm/ThreadHelper.kt
@@ -1,6 +1,5 @@
 package co.touchlab.nmm
 
-import kotlin.native.concurrent.AtomicReference
 import kotlin.native.concurrent.TransferMode
 import kotlin.native.concurrent.Worker
 import kotlin.native.concurrent.freeze
@@ -9,34 +8,27 @@ object ThreadHelper {
     val worker = Worker.start()
 
     fun <T> doInBackgroundUnfrozen(action: () -> T): T {
-        val ex = AtomicReference<Throwable?>(null)
         val future = worker.execute(
             TransferMode.SAFE,
             {
-                val block = if(isStrictMemoryModel)
+                if(isStrictMemoryModel)
                     action.freeze()
                 else
                     action
-                Pair(block, ex)
             }, // No more freeze() call
             {
                 return@execute try {
-                    it.first()
+                    it()
                 } catch (e: Exception) {
-                    it.second.value = e.freeze()
-                    throw e
+                    e
                 }
             }
         )
-        return try {
-            future.result
-        } catch (e: Exception) {
-            val thrown = ex.value
-            if(thrown != null)
-                throw thrown
-            else
-                throw e
-        }
+        val res = future.result
+        if(res is Throwable)
+            throw res
+        else
+            return res as T
     }
 
     val isStrictMemoryModel: Boolean
--- src/nativeTest/kotlin/co/touchlab/nmm/NmmTest.kt
+++ src/nativeTest/kotlin/co/touchlab/nmm/NmmTest.kt
@@ -1,14 +1,22 @@
 package co.touchlab.nmm
 
-import kotlin.native.concurrent.AtomicReference
+import kotlin.native.concurrent.AtomicInt
 import kotlin.test.Test
 
 class NmmTest {
-    @Test
-    fun run() {
+//    @Test
+    fun runFail() {
         var c = 1
         ThreadHelper.doInBackgroundUnfrozen {
             c++
+        }
+    }
+
+    @Test
+    fun run() {
+        val c = AtomicInt(1)
+        ThreadHelper.doInBackgroundUnfrozen {
+            c.value++
         }
     }
 }
